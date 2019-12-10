20de139f6e3d39c38c5d4aadc48b0ee4466d3a34
AdvancedEnglish <talktome@advancedenglish.com.br>
2019-12-10 16:03:48 -0300

    Update word_count.c

--- word_count.c
+++ word_count.c
@@ -1,25 +1,32 @@
 #include "word_count.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 //Accept input and qualify                                                                                                                                                         
-
 int word_count( const char* input_text, word_count_word_t* words)
 {
-    int i = 0;
+  int i = 0, count;
 
-    if ( MAX_WORDS > 20 ) // if input_text is larger then MAX_WORD_LENGTH                                                                                                          
-    {
-      return EXCESSIVE_NUMBER_OF_WORDS; //... return EXCESSIVE                                                                                                                     
-    }
-    if ( MAX_WORD_LENGTH > 50 )  // if words is larger than MAX_WORDS                                                                                                              
-    {
-      return EXCESSIVE_LENGTH_WORD; // return EXCESSIVE                                                                                                                            
-    }
-    else                                // else...                                                                                                                                 
-      for (i = 0; input_text[i] != '\0'; i++)        //loop to count no of words                                                                                                   
-        {
-          if (input_text[i] == ' ')
-            words++;
-        }
-return 0;
+    if (isalpha(input_text[i]))	 //check if first character is alphabetic                                                                                                          
+      {
+        count = 1;
+      }
+     for (i = 0; input_text[i] != '\0'; i++)    //loop to count number of words )                                                                                                  
+       {
+         if (input_text[i] == ' ')           //if character is a ' ', then characters before must've formed a word                                                                 
+           {
+             while (isspace(input_text[i])) // loops through all other spaces                                                                                                      
+               {
+                 i++;
+               }
+             if (input_text[i] != '\0') // if it's not the end of the line, it must be another word                                                                                
+               {
+                 words++;
+               }
+           }
+       }
+     return i;
 }
 
