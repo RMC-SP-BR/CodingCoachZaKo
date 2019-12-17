755a4b2edc79a5a3ea27498b73e4619f71158382
AdvancedEnglish <talktome@advancedenglish.com.br>
2019-12-17 14:58:33 -0300

    Update help_message.c

--- help_message.c
+++ help_message.c
@@ -2,7 +2,14 @@
 
 int main(void)
 {
-    printf("word_count - routine to classify the unique words and their frequency in a test input string\ninputs:\n\tinput_text =  a null-terminated string containing that is analyzed\n\noutputs:\n\twords = allocated structure to record the words found and their frequency\n\tuniqueWords - number of words in the words structure\n\t\treturns a negative number if an error.\n\twords will contain the results up to that point.\n\n"); 
+    printf("word_count - routine to classify the unique words and their frequency in a test input string\n
+           inputs:\n
+           \tinput_text =  a null-terminated string containing that is analyzed\n
+           \noutputs:\n
+           \twords = allocated structure to record the words found and their frequency\n
+           \tuniqueWords - number of words in the words structure\n
+           \t\treturns a negative number if an error.\n
+           \twords will contain the results up to that point.\n\n"); 
 
     return 0;
 }
