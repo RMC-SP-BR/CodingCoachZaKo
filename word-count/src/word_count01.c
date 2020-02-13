e65723350d1da83741f504c312a8f05a204c863f
AdvancedEnglish <brasilrocks@Hotmail.com>
2020-02-13 15:47:11 -0300

    backwards loop
    in this version, the loop counts occurrences of repeated words from the end of the string to the beginning.
    Still, I can't figure out how to properly filter the results.  I can't seem to correlate text->text with text->count.

--- word-count/src/word_count01.c
+++ word-count/src/word_count01.c
@@ -1,60 +1,56 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 int main ()
 {
-  char str [] = "one fisH, two Fish, bLue fish";
+  char str [] = "one fisH, two Fish, bLue fish, one";
   char delimiters[] = " \n\".,;/:!?)(";
-  char *array [60];
+  char *array [80];
   int count = 0;
-  int i;
+  int i, max;
 
-  typedef struct result   //result struct created to store repeated words and its occurrence
+  typedef struct result
   {
-   char text[80];      
+   char text[80];      // allow for the string to be null-terminated
    int count;
+   int maxcount;
   } result_t;
 
   result_t text[80];
+  result_t maxcount;
 
-      //lowercasing the input string
       for (int h = 0; str[h]; h++)
       {
           str[h] = tolower(str[h]);
       }
-      //setting the memory allotted to 0s 
       memset(array, 0, 60);
-      
-      //tokenizing the input
       char *p = strtok (str, delimiters );
       i = 0;
       while (p != NULL) //while it's not the end of the string...
       {
           array[i++] = p;     //put the tokens in the arrays
-          p = strtok (NULL, delimiters);  //tokenize the next input
+          p = strtok (NULL, delimiters);         //tokenize the next input
           count++;                        // count tokens
       }
 
-      //looping input string looking for matches
       for ( i = 0; array[i]; ++i)
       {
-        int matches = 1; // matches start at one
-        for (int j = 0; j < i; ++j)
+        int matches = 0;
+        for (int j = i; j < count; j++)
         {
-          if (!strcmp(array[i], array[j])) //if contents of borh arrays don't match, it's found another word, so ...
+          if (!strcmp(array[i], array[j]))
           {
-            ++matches; // increase matches
+            ++matches;
           }
         }
-        //TODO: print out ONLY the highest occurring match for each individual word in the array
-        
-        strcpy(text->text, array[i]); //copying the matched word into result structure
-        text->count = matches;        //copying the word's occurrence into results structure
-        printf("result: %s\n", text->text); //print statement to validate expected results
-        printf("count: %d\n", matches);     //print statement to validate expected results
-        printf("%d instances of %s in original string\n", matches, array[i]);
-        
+
+        strcpy(text->text, array[i]); //copying the word into text->text
+        text->count = matches;
+        printf("word: %s, count: %d\n" , text->text, text->count);
+
       }
+
 return 0;
 }
